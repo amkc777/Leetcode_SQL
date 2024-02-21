ba76e0f35ff0af7caa56164c4ceef61b7bef6ee9
Mohana Krishna Alla <137847978+amkc777@users.noreply.github.com>
2024-02-21 16:12:10 -0600

    Create 1789_2.sql

---
+++ 1789/1789_2.sql
@@ -0,0 +1,9 @@
+--2nd solution
+select employee_id, department_id
+from employee 
+where primary_flag = 'Y'
+union
+select employee_id, department_id
+from employee
+group by employee_id
+having count(employee_id)=1
